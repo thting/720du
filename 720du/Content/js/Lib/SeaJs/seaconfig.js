6cc373e240f9706e3294603ba2f35ffc7fd28fa8
thting <thing30@yahoo.com>
2017-07-06 21:24:25 +0800

    add

---
+++ 720du/Content/js/Lib/SeaJs/seaconfig.js
@@ -0,0 +1,16 @@
+﻿seajs.config({
+    // 别名配置
+    alias: {
+        'app': 'application',
+        'app2': 'dist/application',
+        'json': '/content/js/json/1.0.2/json',
+        'jquery': 'jquery/jquery/1.10.1/jquery' //使用http://或./或/，会自动判断为绝对路径，否则是相对路径，会加上base目录。
+    },
+
+    // Sea.js 的基础路径，在 seajs@2.3.0 之前，Sea.js 会根据 sea.js 的路径去猜测 base 路径，一般为路径上含有 seajs 字符串的上一级路径。
+    //在 seajs@2.3.0 后，去掉了这个模糊的猜测。我们推荐始终手动设置一个准确的 base 路径。
+    base: '/html/js/',
+
+    // 文件编码
+    charset: 'utf-8'
+});
---
+++ 720du/Controllers/CollectController.cs
@@ -0,0 +1,71 @@
+﻿using du720.BLL;
+using du720.Entity;
+using du720.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace du720.Controllers
+{
+    public class CollectController : Controller
+    {
+        //
+        // GET: /Collect/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost, AjaxFilter]
+        public ActionResult doSave(FormCollection form)
+        {
+            HandleResult hr = new HandleResult();
+            hr.StatsCode = 500;
+            hr.Message = "提交失败";
+
+            if (string.IsNullOrWhiteSpace(form["txtUserName"]))
+            {
+                hr.Message = "请输入联系人。";
+                return Json(hr);
+            }
+
+            if (string.IsNullOrWhiteSpace(form["txtPhone"]))
+            {
+                hr.Message = "请输入手机号。";
+                return Json(hr);
+            }
+            else if (!Regex.IsMatch(form["txtPhone"], "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0,5-9]))\\d{8}$"))
+            {
+                hr.Message = "手机号码格式不对。";
+                return Json(hr);
+            }
+
+            if (string.IsNullOrWhiteSpace(form["txtReason"]))
+            {
+                hr.Message = "请输入推荐原因。";
+                return Json(hr);
+            }
+            else if (form["txtReason"].ToString().Length > 200)
+            {
+                hr.Message = "推荐原因不能超过200个字符。";
+                return Json(hr);
+            }
+
+            int collectType = Convert.ToInt32(form["ddlType"]);
+
+            CollectInfoEntity entityCollect = new CollectInfoEntity();
+            entityCollect.CollectType = collectType;
+            entityCollect.UserName = form["txtUserName"].Trim();
+            entityCollect.Phone = form["txtPhone"].Trim();
+            entityCollect.Reason = form["txtReason"].Trim();
+            entityCollect.AddTime = DateTime.Now;
+            CollectInfoBLL.Insert(entityCollect);
+            hr.StatsCode = 200;
+            hr.Message = "提交成功";
+            return Json(hr);
+        }
+	}
+}

e56ed61da8583093a7da7e2240c2aba080d06420
thting <thing30@yahoo.com>
2017-09-05 23:51:08 +0800

    添加静态

---
+++ 720du/html2/dev/dev/js/index.js
@@ -0,0 +1,32 @@
+﻿
+var style_tool = {};
+style_tool.hint = function (message) {
+    var hint = $("<div class='style-tool-hint' ></div>").text(message);
+    $("body").append(hint);
+    setTimeout(function () {
+        hint.css("opacity", 0);
+    }, 2500)
+    setTimeout(function () {
+        hint.remove()
+    }, 3000);
+}
+
+
+
+
+
+$(".dev-btn").click(function () {
+    var opcy = $(".dev-use").css("opacity");
+    if (opcy == "0") {
+        $(".dev-use").css("opacity", "0.5");
+    } else if (opcy == "0.5") {
+        $(".dev-use").css("opacity", "1");
+    } else if (opcy == "1") {
+        $(".dev-use").css("opacity", "0");
+    } else {
+        $(".dev-use").css("opacity", "0");
+    }
+
+});
+
+
